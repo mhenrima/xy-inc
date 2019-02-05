5d4d1ea1e3b88cd871191323149f6782422d9480
Marcelo Henrique Machado <marcelo.henrique.machado@everis.com>
2019-02-05 12:31:04 -0200

    Commit testes unitario

---
+++ src/test/java/com/marcelo/prova/model/PoiServiceControlTest.java
@@ -0,0 +1,96 @@
+package com.marcelo.prova.model;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.marcelo.prova.DAO.PoiDAO;
+import com.marcelo.prova.service.PoiServiceControl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PoiServiceControlTest {
+	
+	@InjectMocks
+	PoiServiceControl subject;
+	
+	@Spy
+	private List<Poi> listPoi = new ArrayList<>();
+	
+	@Mock
+	private PoiDAO poiDAO; 
+	
+	@Test
+	public void test4ListaPoi() {
+		//preparacao
+		when(poiDAO.findAll()).thenReturn(listPoi);
+				
+		//execucao
+		subject.listaPoi();
+		
+		//verificacao
+		verify(poiDAO).findAll();
+	}
+	
+	@Test
+	public void test4SucessCadastraPoi() {
+		//preparacao
+		Poi poi = new Poi();
+		poi.setName("teste");
+		poi.setX(21);
+		poi.setY(12);
+		
+		//execucacao
+		subject.cadastraPoi(poi);
+		//verificacao
+		verify(poiDAO).save(poi);
+		
+	}
+	@Test(expected = NullPointerException.class) 
+	public void test4XnullCadastraPoi()  {
+		//preparacao
+		Poi poi = new Poi();
+		poi.setName("teste");
+		poi.setX(null);
+		poi.setY(12);
+		
+		//execucacao
+		subject.cadastraPoi(poi);
+		
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void test4YnullCadastraPoi()  {
+		//preparacao
+		Poi poi = new Poi();
+		poi.setName("teste");
+		poi.setX(12);
+		poi.setY(null);
+		
+		//execucacao
+		subject.cadastraPoi(poi);
+		
+	}
+	
+	@Test(expected = NullPointerException.class) 
+	public void test4NameNullCadastraPoi()  {
+		//preparacao
+		Poi poi = new Poi();
+		poi.setName(null);
+		poi.setX(12);
+		poi.setY(41);
+		
+		//execucacao
+		subject.cadastraPoi(poi);
+		
+	}
+}
---
+++ src/test/java/com/marcelo/prova/resources/PoiResourceTest.java
@@ -0,0 +1,86 @@
+package com.marcelo.prova.resources;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.marcelo.prova.DAO.PoiDAO;
+import com.marcelo.prova.model.Poi;
+import com.marcelo.prova.service.PoiServiceControl;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PoiResourceTest {
+	
+	@InjectMocks
+	PoiResource subject;
+	
+	@Mock
+	PoiServiceControl poiServiceControl;
+	
+	@Mock
+	PoiDAO poiDAO; 
+	
+	
+	
+	@Spy
+	private List<Poi> listPoi = new ArrayList<>();
+	
+	
+	
+	@Test
+	public void test4ListaPoi() throws Exception {
+		//preparacao
+		when(poiServiceControl.listaPoi()).thenReturn(listPoi);
+		
+		//execucacao
+		subject.listaPoi();
+		
+		//verificacao
+		verify(poiServiceControl).listaPoi();
+	}
+	
+	@Test
+	public void test4ListaPoiProximo() {
+		//preparacao
+		Poi lanchonete = new Poi(null, "Lanchonete", 27, 12);
+		Poi posto = new Poi(null, "Posto", 31, 18);
+		List<Poi> listaMockada = new ArrayList<>();
+		listaMockada.add(lanchonete);
+		listaMockada.add(posto);
+		when(poiServiceControl.listaPoi()).thenReturn(listaMockada);
+		
+		//execucacao
+		subject.listaPoiProximo(20,10,10);
+		
+		//verificacao
+		verify(poiServiceControl).listaPoi();
+	}
+	
+	@Test
+	public void test4CadastraPoi() {
+		//preparacação
+		Poi poi = mock(Poi.class);
+		when(poiServiceControl.cadastraPoi(poi)).thenReturn(poi);
+	    MockHttpServletRequest request = new MockHttpServletRequest();
+	    RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+	    
+		//execucao
+		subject.cadastraPoi(poi);
+		//verificacao
+		verify(poiServiceControl).cadastraPoi(poi);
+		
+	}
+}
