248655ecc7a122b3f7455624a3c0fc5c056c66b1
Marcelo Henrique Machado <marcelo.henrique.machado@everis.com>
2019-02-07 11:36:24 -0200

    Exceptions

--- src/main/java/com/marcelo/prova/service/PoiServiceControl.java
+++ src/main/java/com/marcelo/prova/service/PoiServiceControl.java
@@ -13,7 +13,8 @@ import com.marcelo.prova.repository.PoiRepository;
 public class PoiServiceControl {
 
 	@Autowired 
-	private PoiRepository poiDAO; 
+	private PoiRepository poiDAO;
+	private String obj; 
 	
 	public List<Poi> listaPoi() {
 		return poiDAO.findAll();
@@ -22,23 +23,24 @@ public class PoiServiceControl {
 	public Poi cadastraPoi(Poi poi) {
 		poi.setId(null);
 	
-		if(poi.getX() == null) {
+		obj = "";
+		if(poi.getX() == null || poi.getX().equals(obj)) {
 			throw new NullPointerException("Eh obrigatorio o preenchimento do parametro X");
 		}
 			
-		if(poi.getY() == null){
+		if(poi.getY() == null || poi.getY().equals(obj)){
 			throw new NullPointerException("Eh obrigatorio o preenchimento do parametro Y");
 		}
 		
-		if(poi.getName() == null) {
+		if(poi.getName() == null || poi.getName() == obj) {
 			throw new NullPointerException("Eh obrigatorio o preenchimento do parametro NOME");
 		}
 		
 		if(poi.getX() < 0) {
-			throw new InvalidParameterException("O valor de X nao pode ser menor que 0");
+			throw new InvalidParameterException("O valor do parametro X nao pode ser menor que 0");
 		}
 		if (poi.getY() < 0) {
-			throw new InvalidParameterException("O valor de Y nao pode ser menor que 0");
+			throw new InvalidParameterException("O valor do parametro Y nao pode ser menor que 0");
 		}
 		
 		return poiDAO.save(poi); 
